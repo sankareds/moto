ff386a641f7b492ab54f62f4c1d33b1e9c15030b
Waldemar Hummer <waldemar.hummer@gmail.com>
2017-06-04 19:18:17 +0200

    add cloudformation models for API Gateway deployments

--- moto/apigateway/exceptions.py
+++ moto/apigateway/exceptions.py
@@ -99,5 +99,12 @@ class ApiKeyNotFoundException(RESTError):
 
     def __init__(self):
         super(ApiKeyNotFoundException, self).__init__(
-            "NotFoundException", "Invalid API Key identifier specified"
-        )
+            "NotFoundException", "Invalid API Key identifier specified")
+
+
+class MethodNotFoundException(RESTError):
+    code = 404
+
+    def __init__(self):
+        super(MethodNotFoundException, self).__init__(
+            "NotFoundException", "Invalid method properties specified")
